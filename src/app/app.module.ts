bd85724f4c03877f533a34b48fb3775a180c054f
Kevin Pimentel <kevin@gymautopilot.com>
2018-02-05 00:08:47 -0500

    Added life declaration.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
 import { AppComponent } from './app.component';
-
+import { Life } from './life.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Life
   ],
   imports: [
     BrowserModule
